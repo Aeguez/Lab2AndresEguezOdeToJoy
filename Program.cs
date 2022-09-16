3117972056007bcc11c67e2a07995ae90bd75a7c
Aeguez <andreseguezdavid13@gmail.com>
2022-09-15 19:09:42 -0500

    commit message

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int A = 440;
+int B = 494;
+int C = 523;
+int D = 587;
+int E = 330;
+int F = 349;
+int G = 392;
+int firstNote = 1000;
+int wholeNote = 1200;
+int halfNote = wholeNote/2;
+int fourthNote = wholeNote/4;
+int doubleNote = 3000;
+
+
+Console.Beep(B, firstNote);
+Console.Beep(B, halfNote);
+Console.Beep(C, halfNote);
+Console.Beep(D, wholeNote);
+
+Console.Beep(D, firstNote);
+Console.Beep(C, halfNote);
+Console.Beep(B, halfNote);
+Console.Beep(A, wholeNote);
+
+Console.Beep(G, firstNote);
+Console.Beep(G, halfNote);
+Console.Beep(A, halfNote);
+Console.Beep(B, wholeNote);
+
+Console.Beep(B, firstNote);
+Console.Beep(A, halfNote);
+Console.Beep(A, doubleNote);
+
+Console.Beep(B, firstNote);
+Console.Beep(B, halfNote);
+Console.Beep(C, halfNote);
+Console.Beep(D, wholeNote);
+
+Console.Beep(D, firstNote);
+Console.Beep(C, halfNote);
+Console.Beep(B, halfNote);
+Console.Beep(A, wholeNote);
+
+Console.Beep(G, firstNote);
+Console.Beep(G, halfNote);
+Console.Beep(A, halfNote);
+Console.Beep(B, wholeNote);
+
+Console.Beep(A, firstNote);
+Console.Beep(G, halfNote);
+Console.Beep(G, doubleNote);
+
+Console.Beep(A, firstNote);
+Console.Beep(A, halfNote);
+Console.Beep(B, halfNote);
+Console.Beep(G, wholeNote);
+
+Console.Beep(A, firstNote);
+Console.Beep(B, fourthNote);
+Console.Beep(C, fourthNote);
+Console.Beep(B, halfNote);
+Console.Beep(G, wholeNote);
+
+Console.Beep(A, firstNote);
+Console.Beep(B, fourthNote);
+Console.Beep(C, fourthNote);
+Console.Beep(B, halfNote);
+Console.Beep(A, wholeNote);
+
+Console.Beep(G, firstNote);
+Console.Beep(A, halfNote);
+Console.Beep(D, doubleNote);
+
+Console.Beep(B, firstNote);
+Console.Beep(B, halfNote);
+Console.Beep(C, halfNote);
+Console.Beep(D, wholeNote);
+
+Console.Beep(D, firstNote);
+Console.Beep(C, halfNote);
+Console.Beep(B, halfNote);
+Console.Beep(A, wholeNote);
+
+Console.Beep(G, firstNote);
+Console.Beep(G, halfNote);
+Console.Beep(A, halfNote);
+Console.Beep(B, wholeNote);
+
+Console.Beep(A, firstNote);
+Console.Beep(G, wholeNote);
+Console.Beep(G, doubleNote);
